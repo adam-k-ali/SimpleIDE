2d6bcda92e65c237ef71ba6de46bc20e9bede62e
Adam Ali <adamka2000@outlook.com>
2023-12-18 14:11:50 +0000

    Add key input to canvas

--- src/main/java/com/adamkali/simpleide/App.java
+++ src/main/java/com/adamkali/simpleide/App.java
@@ -1,8 +1,58 @@
 package com.adamkali.simpleide;
 
-public class App {
+import com.adamkali.simpleide.codearea.CodeArea;
 
+import javax.swing.*;
+
+public class App {
+    private JFrame mainWindow;
+
+    /**
+     * Creates the JFrame and sets the size and title
+     */
+    public void init() {
+        mainWindow = new JFrame("SimpleIDE");
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(800, 600);
+        mainWindow.setLocationRelativeTo(null);
+
+
+        run();
+    }
+
+    public void run() {
+        CodeArea codeArea = new CodeArea();
+        mainWindow.add(codeArea);
+
+        // Set mainWindow to be visible
+        mainWindow.setVisible(true);
+
+        // Initial paint of the codeArea
+//        codeArea.paint(codeArea.getGraphics());
+
+        // Create thread to repaint the codeArea 60 times per second
+        Thread thread = new Thread(() -> {
+            while (true) {
+                codeArea.update();
+                try {
+                    Thread.sleep(1000 / 60);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+
+    }
+
+
+    /**
+     * Main method
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App app = new App();
+        app.init();
     }
 }
---
+++ src/main/java/com/adamkali/simpleide/codearea/CodeArea.java
@@ -0,0 +1,85 @@
+package com.adamkali.simpleide.codearea;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+public class CodeArea extends Canvas implements KeyListener {
+    private static final int MARGIN = 8;
+    private static final int LINE_HEIGHT = 20;
+
+    private ArrayList<String> lines;
+
+    public CodeArea() {
+        // Setup CodeArea properties
+        lines = new ArrayList<>();
+
+        // Setup listeners
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
+        // Setup canvas graphics
+        setBackground(Color.WHITE);
+
+        // FIXME: For testing purposes only
+        lines.add("Hello World!");
+    }
+
+    /**
+     * Updates CodeArea properties,
+     * such as cursor position, text, etc.
+     */
+    public void update() {
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        for (int i = 0; i < lines.size(); i++) {
+            g.drawString(lines.get(i), MARGIN, MARGIN + (i + 1) * LINE_HEIGHT);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println(e.getKeyChar());
+        switch (e.getKeyChar()) {
+            case KeyEvent.VK_ENTER:
+                lines.add("");
+                break;
+            case KeyEvent.VK_BACK_SPACE:
+                // Don't allow backspace if we're on the first, empty line
+                if (lines.size() == 1 && lines.get(0).isEmpty() || lines.isEmpty()) {
+                    break;
+                }
+
+                if (!lines.get(lines.size() - 1).isEmpty()) {
+                    lines.set(lines.size() - 1, lines.get(lines.size() - 1).substring(0, lines.get(lines.size() - 1).length() - 1));
+                } else {
+                    lines.remove(lines.size() - 1);
+                }
+                break;
+            default:
+                // If the file is empty, add a new line
+                if (lines.isEmpty()) {
+                    lines.add("");
+                }
+
+                lines.set(lines.size() - 1, lines.get(lines.size() - 1) + e.getKeyChar());
+                break;
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
