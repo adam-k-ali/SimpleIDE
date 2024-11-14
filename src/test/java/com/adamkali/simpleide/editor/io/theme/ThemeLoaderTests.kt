7a1211596d869d4be8d10ed3fd117c6f2d312291
Adam Ali <adamka2000@outlook.com>
2024-11-14 07:25:21 +0000

    Create ThemeLoader.loads test

---
+++ src/test/java/com/adamkali/simpleide/editor/io/theme/ThemeLoaderTests.kt
@@ -0,0 +1,35 @@
+package com.adamkali.simpleide.editor.io.theme
+
+import org.junit.jupiter.api.Assertions
+
+class ThemeLoaderTests {
+
+    @org.junit.jupiter.api.Test
+    fun loads() {
+        val colorPropertyString: String = "{\n" +
+                "  \"current_line_highlight\": {\n" +
+                "    \"color\": {\n" +
+                "      \"background\": [\n" +
+                "        30,\n" +
+                "        30,\n" +
+                "        30,\n" +
+                "        255\n" +
+                "      ],\n" +
+                "      \"foreground\": [\n" +
+                "        200,\n" +
+                "        200,\n" +
+                "        200,\n" +
+                "        255\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  }\n" +
+                "}"
+
+        val colorMap =
+            mapOf("current_line_height" to ColorProperty(intArrayOf(30, 30, 30, 255), intArrayOf(200, 200, 200, 255)))
+        val expectedTheme = Theme(colorMap)
+
+        val loadedTheme = ThemeLoader.loads(colorPropertyString)
+        Assertions.assertEquals(expectedTheme, loadedTheme)
+    }
+}
