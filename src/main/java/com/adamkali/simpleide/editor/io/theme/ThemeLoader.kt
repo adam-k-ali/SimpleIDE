bde8cce4fa7c8bdcd38ffd3e779a08e93fb73b4e
Adam Ali <adamka2000@outlook.com>
2024-11-14 19:29:13 +0000

    Remove unused code

--- src/main/java/com/adamkali/simpleide/editor/io/theme/ThemeLoader.kt
+++ src/main/java/com/adamkali/simpleide/editor/io/theme/ThemeLoader.kt
@@ -7,9 +7,6 @@ import com.google.gson.reflect.TypeToken
 object ThemeLoader {
     fun loads(s: String): Theme {
         val propertyMapType = object : TypeToken<Map<String, ThemeProperty>>() {}.type
-        val colorProperty = ColorProperty(intArrayOf(0, 0, 0, 255), intArrayOf(255, 255, 255, 255))
-
-        val colorMap = mapOf("current_line_height" to colorProperty)
         val properties: Map<String, ThemeProperty> = Gson().fromJson(s, propertyMapType)
         return Theme(properties)
     }
