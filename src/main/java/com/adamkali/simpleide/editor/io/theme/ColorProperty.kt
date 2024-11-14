315eaf0c57e2128153d32f096bcefbb111c672ef
Adam Ali <adamka2000@outlook.com>
2024-11-14 07:24:10 +0000

    Add ColorProperty dataclass

---
+++ src/main/java/com/adamkali/simpleide/editor/io/theme/ColorProperty.kt
@@ -0,0 +1,13 @@
+package com.adamkali.simpleide.editor.io.theme
+
+data class ColorProperty(
+    val foreground: IntArray,
+    val background: IntArray
+) {
+    override fun toString(): String {
+        return ("{" +
+                "foreground=[" + foreground[0] + ", " + foreground[1] + ", " + foreground[2] + ", " + foreground[3] + "], " +
+                "background=[" + background[0] + ", " + background[1] + ", " + background[2] + ", " + background[3] + "]" +
+                "}");
+    }
+}
